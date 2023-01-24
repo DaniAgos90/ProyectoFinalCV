de0653221f9644d5139b3a1c7e5d0465fb44cba7
Daniel Agostinelli <daniagos_90@hotmail.com>
2023-01-24 19:47:46 -0300

    modificando el CSS para el proyecto

--- CurriculumJs.js
+++ CurriculumJs.js
@@ -3,16 +3,28 @@ document.getElementById("datos_personales_ancla").addEventListener("click", func
     document.getElementById("datos_personales").style.display = "inline"
     document.getElementById("estudios_cursados").style.display = "none"
     document.getElementById("experiencia_laboral").style.display = "none"
+    document.getElementById("parrafo_presenación").style.display ="none"
+    document.getElementById("logo_persona").style.display = "inline"
+    document.getElementById("logo_estudios").style.display = "none"
+    document.getElementById("logo_trabajo").style.display = "none"
 });
 
 document.getElementById("estudios_cursados_ancla").addEventListener("click", function(){
     document.getElementById("estudios_cursados").style.display = "inline"
     document.getElementById("datos_personales").style.display = "none"
     document.getElementById("experiencia_laboral").style.display = "none"
+    document.getElementById("parrafo_presenación").style.display ="none"
+    document.getElementById("logo_estudios").style.display = "inline"
+    document.getElementById("logo_persona").style.display = "none"
+    document.getElementById("logo_trabajo").style.display = "none"
 });
 
 document.getElementById("experiencia_laboral_ancla").addEventListener("click", function(){
     document.getElementById("experiencia_laboral").style.display = "inline"
     document.getElementById("datos_personales").style.display = "none"
     document.getElementById("estudios_cursados").style.display = "none"
+    document.getElementById("parrafo_presenación").style.display ="none"
+    document.getElementById("logo_trabajo").style.display = "inline"
+    document.getElementById("logo_estudios").style.display = "none"
+    document.getElementById("logo_persona").style.display = "none"
 });
